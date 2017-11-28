da214275c6da6b728646d9233476d75686945bd1
Dan Steshenko <dan.steshenko@gmail.com>
2017-11-29 01:34:45 +0300

    move to https

--- app/components/report-chart.js
+++ app/components/report-chart.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import {
-  set,
   setProperties,
   get,
   computed
--- app/services/ajax.js
+++ app/services/ajax.js
@@ -4,6 +4,6 @@ export default AjaxService.extend({
   headers: {
       'Authorization': 'Bearer eyJpYXQiOjE1MDkzNDEzNzgsImFsZyI6IkhTMjU2IiwiZXhwIjoxNTQwNDQ1Mzc4fQ.eyJpZCI6MTI2NiwicGVybWlzc2lvbnMiOlsiQmFzaWMgUmVwb3J0cyJdLCJnYXRlX2lkIjotMTQzLCJleHAiOiIyMDE4LTEwLTI1IDA1OjI5OjM4In0.dD_So803EIkRM86ARm1RxPy85lzNse2hNaPMjndkPpg'
   },
-  host: 'http://backoffice.aviasales.ru',
+  host: 'https://backoffice.aviasales.ru',
   namespace: '/api'
 });
